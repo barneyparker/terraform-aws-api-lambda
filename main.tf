bebafdc7658a85f6f6dd75dfda4cb1098d478061
Barney Parker <barney.parker@claranet.uk>
2020-04-13 16:16:28 +0100

    Use terraform-aws-api-generic parent module

--- main.tf
+++ main.tf
@@ -1,37 +1,17 @@
-resource "aws_api_gateway_method" "method" {
-  rest_api_id        = var.api_id
+module "lambda_proxy" {
+  source = "github.com/barneyparker/terraform-aws-api-generic"
+
+  api_id        = var.api_id
   resource_id        = var.resource_id
   http_method        = var.http_method
   authorization      = var.authorization
-  request_parameters = var.request_parameters
-}
+  method_request_parameters = var.method_request_parameters
+  integration_request_parameters = var.integration_request_parameters
 
-resource "aws_api_gateway_integration" "integration" {
-  rest_api_id             = var.api_id
-  resource_id             = var.resource_id
-  http_method             = aws_api_gateway_method.method.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = var.invoke_arn
+
   request_templates = var.request_templates
-}
-
-resource "aws_api_gateway_integration_response" "integration_response" {
-  count = length(var.responses)
-
-  rest_api_id        = var.api_id
-  resource_id        = var.resource_id
-  http_method        = aws_api_gateway_method.method.http_method
-  status_code        = var.responses[count.index].status_code
-  selection_pattern  = var.responses[count.index].selection_pattern
-  response_templates = var.responses[count.index].templates
-}
-
-resource "aws_api_gateway_method_response" "method_response" {
-  count = length(var.responses)
-
-  rest_api_id = var.api_id
-  resource_id = var.resource_id
-  http_method = aws_api_gateway_method.method.http_method
-  status_code = var.responses[count.index].status_code
+  responses         = var.responses
 }
